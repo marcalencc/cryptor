fdbf0d519219d4f13d7bbb81ad68b89092104024
Marc Alen Cuevas <marcalencc@gmail.com>
2018-01-22 09:00:29 +0800

    - Monitoring panel layout

---
+++ Cryptor/MonitoringPanel.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cryptor
+{
+    /// <summary>
+    /// Interaction logic for MonitoringPanel.xaml
+    /// </summary>
+    public partial class MonitoringPanel : UserControl
+    {
+        public MonitoringPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
